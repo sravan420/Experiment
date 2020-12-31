6ce309e63d56a50a82cad597fb22f313cf849481
sravan420 <43325481+sravan420@users.noreply.github.com>
2020-12-31 17:50:48 -0600

    Update FlatmapDemo-latest.java

--- FlatmapDemo.java
+++ FlatmapDemo.java
@@ -30,13 +30,42 @@ public class FlatmapDemo {
         System.out.println(str.orElse("entered valued found is empty"));
 
 
-        int a[]={1,2,4,6,3,2};
+        int a[]={1,2,3,2,8};
         OptionalInt min= IntStream.of(a).min();
+        System.out.println(min);
         OptionalInt max= IntStream.of(a).max();
+        System.out.println(max);
         System.out.println(min.orElse(0));
         System.out.println(max.getAsInt());
 
 
+        
+        System.out.println("**********local changes");
+        int changedarray[]={100,200};
+        OptionalInt minim= IntStream.of(changedarray).min();
+        System.out.println("Local changes:" + minim);
+
+        int maxarray[]={94,48,75,89};
+        OptionalInt maximum= IntStream.of(maxarray).max();
+        System.out.println("Local changes:" + maximum);
+
+        
+        
+        System.out.println("**********GITHUBREBASE**********");
+        int changedarray1[]={100,300,400};
+        OptionalInt minim1= IntStream.of(changedarray1).min();
+        System.out.println(minim1);
+        
+        int maxarray1[]={94,48,75,89};
+        OptionalInt maximum1= IntStream.of(maxarray1).max();
+        System.out.println(maximum1);
+        
+       
+        
+        System.out.println("hey add some changes for rebase from github");
+         System.out.println("hey adding some changes for rebase from github");
+
+        System.out.println("hey added some for rebase from local");
 
         students.stream().sorted(Comparator.comparingInt(Student::getRollno).reversed()).limit(3).map(st->st.getName()).forEach(System.out::println);
 
